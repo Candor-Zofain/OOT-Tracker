143cccfb9664d47894b47149c98c329bbcaf7e0c
Candor-Zofain <czofain@gmail.com>
2020-05-05 16:03:38 -0400

    Created list box. Able to transition between areas.

--- js/checkChestLogic.js
+++ js/checkChestLogic.js
@@ -7,6 +7,67 @@ function CheckForLogic() {
 };
 setTimeout(CheckForLogic, 10);
 
+let chestListElement = document.getElementById('chest-list');
+
+function CreateChestAreaLine(chestLinesElement, area) {
+    let div = document.createElement('div');
+    div.className = "chest-line";
+
+    let h2 = document.createElement('h2');
+    h2.innerText = area;
+
+    if (area === '(back)') {
+        div.onclick = GoToAreaList;
+    } else {
+        div.onclick = () => {
+            GoToChestList(area)
+        };
+    }
+
+    div.appendChild(h2);
+    chestLinesElement.appendChild(div);
+}
+
+function CreateChestLine(areaChestLinesElement, chestName, chest) {
+    let div = document.createElement('div');
+    div.className = "chest-line";
+
+    let h2 = document.createElement('h2');
+    h2.innerText = chestName;
+
+    div.appendChild(h2);
+    areaChestLinesElement.appendChild(div);
+}
+
+function GoToAreaList() {
+    chestListElement.getElementsByClassName('header')[0].innerHTML = `
+        <h1>Hyrule</h1>
+    `;
+
+    let chestLinesElement = document.getElementById('chest-lines');
+    chestLinesElement.innerHTML = '';
+    Object.keys(chestAreaLogic.chests).forEach(area => {
+        CreateChestAreaLine(chestLinesElement, area);
+    });
+}
+
+function GoToChestList(area) {
+    chestListElement.getElementsByClassName('header')[0].innerHTML = `
+        <h1>${area}</h1>
+    `;
+
+    let chestLinesElement = document.getElementById('chest-lines');
+    chestLinesElement.innerHTML = '';
+    CreateChestAreaLine(chestLinesElement, '(back)');
+
+    let chests = chestAreaLogic.chests[area];
+    Object.keys(chests).forEach(chestName => {
+        let chest = chests[chestName];
+        CreateChestLine(chestLinesElement, chestName, chest);
+    })
+}
+
 let InitLogic = function () {
-    console.log('logic loaded');
+    // console.log(chestAreaLogic);
+    GoToAreaList();
 };
